fe2b5caaa82ed49ec318abec03eb1e9d7d6c8c17
Fareed Mohammed <fareed.mohammed.uk@gmail.com>
2019-02-13 12:30:02 +0000

    Add files via upload

---
+++ src/VendingMachine.UnitTests/Helpers/AssertException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VendingMachine.UnitTests.Helpers
+{
+    public class AssertException
+    {
+        public static void Throws<T>(Action action, string expectedMessage) where T : Exception
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (T exc)
+            {
+                Assert.AreEqual(expectedMessage, exc.Message);
+
+                return;
+            }
+
+            Assert.Fail("Exception of type {0} should be thrown.", typeof(T));
+        }
+    }
+}
